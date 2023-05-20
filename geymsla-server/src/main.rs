b9b2d54c9f5bed68a940c64eae59e0da0d54c379
Graham Keenan <graham.keenan01@bbc.co.uk>
2023-05-20 21:56:26 +0100

    basic client / server talking

--- geymsla-server/src/main.rs
+++ geymsla-server/src/main.rs
@@ -1,35 +1,63 @@
-use std::collections::{HashMap, HashSet};
-use tokio::net::TcpListener;
+use std::collections::HashMap;
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpListener,
+    sync::Mutex,
+};
+
+use std::sync::Arc;
 
 const DEFAULT_GEYMSLA_PORT: usize = 9876;
 
 pub struct Geymsla {
     pub strings: HashMap<String, String>,
-    pub lists: HashMap<String, Vec<String>>,
-    pub sets: HashMap<String, HashSet<String>>,
-    pub hashes: HashMap<String, HashMap<String, String>>,
 }
 
 impl Geymsla {
     pub fn empty() -> Self {
         Self {
             strings: HashMap::default(),
-            lists: HashMap::default(),
-            sets: HashMap::default(),
-            hashes: HashMap::default(),
         }
+    }
+
+    pub fn insert_string(&mut self, string: &str) -> std::io::Result<String> {
+        println!("Inserting string: {string}");
+
+        Ok(string.to_string())
     }
 }
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let mut geymsla = Geymsla::empty();
+    let geymsla = Arc::new(Mutex::new(Geymsla::empty()));
     let addr = format!("0.0.0.0:{}", DEFAULT_GEYMSLA_PORT);
     let listener = TcpListener::bind(&addr).await?;
 
     println!("Started Geymsla server");
     loop {
-        let (socket, _) = listener.accept().await?;
-        println!("Accepted new client: {}", socket.peer_addr()?);
+        let (mut client, _) = listener.accept().await?;
+        let store = Arc::clone(&geymsla);
+
+        println!("Accepted new client: {}", &client.peer_addr()?);
+
+        tokio::spawn(async move {
+            let mut buffer = vec![0; 4096];
+            let n_bytes = client
+                .read(&mut buffer)
+                .await
+                .expect("unable to read from client");
+
+            if n_bytes == 0 {
+                return;
+            }
+
+            let msg = String::from_utf8(buffer).expect("unable to convert buffer to message");
+            let mut geymsla = store.lock().await;
+            let _ = geymsla.insert_string(msg.as_ref());
+            client
+                .write_all(b"success")
+                .await
+                .expect("unable to response to client");
+        });
     }
 }
