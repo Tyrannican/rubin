c85810c10b255ec3d4f21fc97a3aa54c7367aa1e
Graham Keenan <graham.keenan@outlook.com>
2023-05-23 19:38:24 +0100

    moved client handler to own file

---
+++ geymsla-server/src/client.rs
@@ -0,0 +1,69 @@
+use std::sync::Arc;
+
+use geymsla_lib::{
+    parser::{Message, OpCode},
+    store::Vault,
+};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpStream,
+    sync::Mutex,
+};
+
+async fn send_response(client: &mut TcpStream, code: OpCode, msg: &str) {
+    let response = format!("{}: {}\n", code.to_string(), msg);
+    client
+        .write_all(response.as_bytes())
+        .await
+        .expect("unable to response to client");
+}
+
+async fn read_from_client(client: &mut TcpStream) -> String {
+    let mut buffer = vec![0; 4096];
+    let n_bytes = client
+        .read(&mut buffer)
+        .await
+        .expect("unable to read from client");
+
+    if n_bytes == 0 {
+        return String::from("");
+    }
+
+    let msg = String::from_utf8_lossy(&buffer[..n_bytes]);
+    msg.trim_end().to_string()
+}
+
+pub async fn handler(mut client: TcpStream, store: Arc<Mutex<Vault>>) {
+    let msg = read_from_client(&mut client).await;
+
+    let message = match Message::new(msg.as_str()) {
+        Ok(m) => m,
+        Err(e) => {
+            eprintln!("unable to create message: {}", e);
+            return;
+        }
+    };
+
+    let mut vault = store.lock().await;
+    match message.op {
+        OpCode::SSet => {
+            let key = &message.contents[0];
+            let value = &message.contents[1..].join(" ");
+
+            if let Ok(_) = vault.insert_string(key, value) {
+                send_response(&mut client, message.op, "OK").await;
+            }
+        }
+        OpCode::SGet => {
+            let key = &message.contents[0];
+            if let Ok(value) = vault.get_string(key) {
+                send_response(&mut client, message.op, &value).await;
+            }
+        }
+        OpCode::Noop => {
+            send_response(&mut client, message.op, "invalid command").await;
+        }
+    }
+
+    dbg!(&vault.strings);
+}
--- geymsla-server/src/main.rs
+++ geymsla-server/src/main.rs
@@ -1,70 +1,12 @@
-use geymsla_lib::{
-    parser::{Message, OpCode},
-    store::Vault,
-};
-use tokio::{
-    io::{AsyncReadExt, AsyncWriteExt},
-    net::{TcpListener, TcpStream},
-    sync::Mutex,
-};
+mod client;
+
+use client::handler;
+use geymsla_lib::store::Vault;
+use tokio::{net::TcpListener, sync::Mutex};
 
 use std::sync::Arc;
 
 const DEFAULT_GEYMSLA_PORT: usize = 9876;
-
-async fn send_response(client: &mut TcpStream, code: OpCode, msg: &str) {
-    let response = format!("{}: {}\n", code.to_string(), msg);
-    client
-        .write_all(response.as_bytes())
-        .await
-        .expect("unable to response to client");
-}
-
-async fn handler(mut client: TcpStream, store: Arc<Mutex<Vault>>) {
-    let mut buffer = vec![0; 4096];
-    let n_bytes = client
-        .read(&mut buffer)
-        .await
-        .expect("unable to read from client");
-
-    if n_bytes == 0 {
-        return;
-    }
-
-    let msg = String::from_utf8_lossy(&buffer[..n_bytes]);
-    let msg = msg.trim_end();
-
-    let message = match Message::new(msg) {
-        Ok(m) => m,
-        Err(e) => {
-            eprintln!("unable to create message: {}", e);
-            return;
-        }
-    };
-
-    let mut vault = store.lock().await;
-    match message.op {
-        OpCode::SSet => {
-            let key = &message.contents[0];
-            let value = &message.contents[1..].join(" ");
-
-            if let Ok(_) = vault.insert_string(key, value) {
-                send_response(&mut client, message.op, "OK").await;
-            }
-        }
-        OpCode::SGet => {
-            let key = &message.contents[0];
-            if let Ok(value) = vault.get_string(key) {
-                send_response(&mut client, message.op, &value).await;
-            }
-        }
-        OpCode::Noop => {
-            send_response(&mut client, message.op, "invalid command").await;
-        }
-    }
-
-    dbg!(&vault.strings);
-}
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
