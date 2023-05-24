d0a0c5f446c3fc89415593c1507c04ae21da4cb1
Graham Keenan <graham.keenan01@bbc.co.uk>
2023-05-24 15:54:55 +0100

    client: removed unused code in main

--- rubin-client/src/main.rs
+++ rubin-client/src/main.rs
@@ -3,10 +3,5 @@ use tokio::time::{sleep, Duration};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let client = RubinClient::new("127.0.0.1", 9876);
-    client.insert_string("user:2000", "rebecca").await?;
-    sleep(Duration::from_millis(2000)).await;
-    let response = client.get_string("user:2000").await?;
-    println!("Received: {}", response);
     Ok(())
 }
