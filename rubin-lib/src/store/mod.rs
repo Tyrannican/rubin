fe2993dc33e51b46641c530761226d42ec22959d
Graham Keenan <graham.keenan@outlook.com>
2023-05-25 21:39:19 +0100

    lib: added method to memstore to convert it to a persistent store

--- rubin-lib/src/store/mod.rs
+++ rubin-lib/src/store/mod.rs
@@ -1,5 +1,7 @@
 pub mod persistence;
 
+use persistence::PersistentStore;
+
 use serde::{Deserialize, Serialize};
 
 use std::collections::HashMap;
@@ -22,11 +24,16 @@ impl MemStore {
     }
 
     pub fn get_string(&self, key: &str) -> io::Result<String> {
-        dbg!(format!("Retrieving value for {}", key));
         if let Some(value) = self.strings.get(key) {
             return Ok(value.clone());
         }
 
         Ok("".to_string())
+    }
+
+    pub async fn to_persistent_store(self, storage_loc: &str) -> PersistentStore {
+        let mut ps = PersistentStore::new(storage_loc).await;
+        ps.store = self;
+        ps
     }
 }
