bfa74e043af33f503bac41bb921c18434c11a0d0
Graham Keenan <graham.keenan@outlook.com>
2023-05-29 18:22:22 +0100

    test: added tests for filehandling

--- src/store/persistence/file_handling.rs
+++ src/store/persistence/file_handling.rs
@@ -15,6 +15,7 @@ pub async fn create_directory<P: AsRef<Path>>(location: P) -> Result<PathBuf> {
 
 pub async fn load_store(path: &PathBuf) -> Result<String> {
     let fp = path.join(STORAGE_FILE);
+
     let mut file = fs::OpenOptions::new()
         .create(true)
         .read(true)
@@ -39,4 +40,95 @@ pub async fn write_store(path: &PathBuf, store: &MemStore) -> Result<()> {
     file.write_all(&raw.as_bytes()).await?;
 
     Ok(())
+}
+
+#[cfg(test)]
+mod fh_tests {
+    use super::*;
+    use std::collections::HashMap;
+    use std::io;
+    use std::path::PathBuf;
+    use tempdir::TempDir;
+
+    fn create_test_directory() -> io::Result<PathBuf> {
+        let td = TempDir::new("teststore")?;
+        Ok(td.path().to_path_buf())
+    }
+
+    #[tokio::test]
+    async fn creating_a_directory() -> io::Result<()> {
+        let td = create_test_directory()?;
+        let expected = td.join("should_be_a_new_dir");
+
+        let result = create_directory(&expected).await?;
+
+        assert_eq!(expected, result);
+        assert!(result.exists());
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn loading_an_empty_store() -> io::Result<()> {
+        let td = create_test_directory()?;
+        let rubinstore = td.join(STORAGE_FILE);
+        create_directory(&td).await?;
+
+        let result = load_store(&td).await?;
+        assert!(result.len() == 0);
+        assert!(rubinstore.exists());
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn loading_an_existing_store() -> io::Result<()> {
+        let td = create_test_directory()?;
+        let rubinstore = td.join(STORAGE_FILE);
+        create_directory(&td).await?;
+
+        let mut f = tokio::fs::File::create(&rubinstore).await?;
+        f.write_all(b"some_content").await?;
+
+        let result = load_store(&td).await?;
+        assert!(result.len() != 0);
+        assert_eq!(result, "some_content");
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn write_a_store_out() -> io::Result<()> {
+        let td = create_test_directory()?;
+        let rubinstore = td.join("rubinstore.json");
+        create_directory(&td).await?;
+
+        let mut ms = MemStore::new();
+        ms.insert_string("key1", "value1")?;
+
+        write_store(&td, &ms).await?;
+
+        assert!(rubinstore.exists());
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn write_a_store_out_and_compare() -> io::Result<()> {
+        let td = create_test_directory()?;
+        let rubinstore = td.join("rubinstore.json");
+        create_directory(&td).await?;
+
+        let mut ms = MemStore::new();
+        ms.insert_string("key1", "value1")?;
+
+        write_store(&td, &ms).await?;
+
+        assert!(rubinstore.exists());
+
+        let contents = load_store(&td).await?;
+        let hs: HashMap<String, HashMap<String, String>> = serde_json::from_str(&contents)?;
+        let strings = hs.get("strings").unwrap();
+        assert!(*strings == ms.strings);
+
+        Ok(())
+    }
 }
