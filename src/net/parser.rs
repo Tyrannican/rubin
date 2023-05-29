1676fa3b5cc4c256e9741e0d0ba561bb1e8552aa
Graham Keenan <graham.keenan@outlook.com>
2023-05-29 18:52:49 +0100

    test: adding tests for the parser

--- src/net/parser.rs
+++ src/net/parser.rs
@@ -29,6 +29,7 @@ impl std::fmt::Display for Operation {
     }
 }
 
+#[derive(Debug, Clone, PartialEq)]
 pub struct Message {
     pub op: Operation,
     pub args: Vec<String>,
@@ -53,6 +54,7 @@ impl Message {
                     valid = true;
                 }
             }
+            Operation::Noop => valid = true,
             _ => {}
         }
 
@@ -65,6 +67,7 @@ pub fn create_request(op_code: Operation, args: Vec<String>) -> String {
 }
 
 pub fn parse_request(req: &str) -> Result<Message, MessageError> {
+    // FIXME: Bounds checking
     let r_split = req
         .split("::")
         .map(|s| s.to_string())
@@ -92,4 +95,98 @@ pub fn parse_response(msg: &str) -> String {
     }
 
     return resp[1].to_string();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn create_appropriate_operation() {
+        let op_codes = vec!["SET", "GET", "SOMETHING"];
+        for op in op_codes {
+            let code: Operation = Operation::from_str(op);
+
+            match op {
+                "SET" => assert!(code == Operation::StringSet),
+                "GET" => assert!(code == Operation::StringGet),
+                _ => assert!(code == Operation::Noop),
+            }
+        }
+    }
+
+    #[test]
+    fn validation_string_set_message() {
+        let mut m = Message {
+            op: Operation::StringSet,
+            args: vec!["arg1".to_string(), "arg2".to_string()],
+        };
+
+        assert!(m.validate());
+
+        m.args.pop();
+        assert!(!m.validate());
+    }
+
+    #[test]
+    fn validation_string_get_message() {
+        let mut m = Message {
+            op: Operation::StringGet,
+            args: vec!["arg1".to_string()],
+        };
+
+        assert!(m.validate());
+
+        m.args.push("arg2".to_string());
+        assert!(!m.validate());
+    }
+
+    #[test]
+    fn validation_noop_message() {
+        let m = Message {
+            op: Operation::Noop,
+            args: vec![],
+        };
+
+        assert!(m.validate());
+    }
+
+    #[test]
+    fn create_appropriate_request() {
+        let ops = vec![Operation::StringSet, Operation::StringGet];
+        for op in ops {
+            let args = match op {
+                Operation::StringSet => vec!["arg1".to_string(), "arg2".to_string()],
+                Operation::StringGet => vec!["arg1".to_string()],
+                _ => vec![],
+            };
+            let expected = format!("{}::{}", op, args.join(" "));
+            let request = create_request(op, args);
+            assert_eq!(expected, request);
+        }
+    }
+
+    #[test]
+    fn parse_requests_correctly() {
+        let request = "SET::arg1 arg2";
+        let result = parse_request(request);
+
+        match result {
+            Ok(message) => {
+                let expected = Message {
+                    op: Operation::StringSet,
+                    args: vec!["arg1".to_string(), "arg2".to_string()],
+                };
+
+                assert_eq!(message, expected);
+            }
+            Err(_) => {}
+        }
+    }
+
+    #[test]
+    fn parse_invalid_requests() {
+        let request = "SGET:argumetns blahg";
+        let result = parse_request(request);
+    }
 }
